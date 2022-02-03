851fda8115e40d8b75cbfc3816fa74386a2dddaa
Booreda123 <booreda58@gmail.com>
2022-02-03 13:09:05 +0100

    sd

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -46,7 +46,7 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+        I believe that my purpose In life is to right code and make apps and software that will help people all over the wold make their lives easier 
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
